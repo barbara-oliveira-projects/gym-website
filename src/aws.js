ed0ce175bcbcd1a60a340350b0b42470622163be
Bárbara Oliveira <barbaraoliveiradasilva316@gmail.com>
2024-06-11 14:29:53 -0300

    Fix eslint warn

--- src/aws.js
+++ src/aws.js
@@ -3,7 +3,9 @@ import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
   UserPoolId: process.env.COGNITO_USER_POOL_ID,
-  ClientId: process.env.COGNITO_USER_POOL_CLIENT_ID
+  ClientId: process.env.COGNITO_CLIENT_ID
 };
 
-export default new CognitoUserPool(poolData);
+const userPool = new CognitoUserPool(poolData);
+
+export default userPool;
