6cbc53454903466db171be3950385de056868823
Bárbara Oliveira <barbaraoliveiradasilva316@gmail.com>
2024-06-11 14:37:00 -0300

    Fix var name

--- src/aws.js
+++ src/aws.js
@@ -3,7 +3,7 @@ import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
   UserPoolId: process.env.COGNITO_USER_POOL_ID,
-  ClientId: process.env.COGNITO_CLIENT_ID
+  ClientId: process.env.COGNITO_USER_POOL_CLIENT_ID
 };
 
 const userPool = new CognitoUserPool(poolData);
