ab5479828cf641df743fce64e0d16c0aa57a752d
Bárbara Oliveira <barbaraoliveiradasilva316@gmail.com>
2024-06-16 12:27:08 -0300

    Fix header issues

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Container, Carousel, Form, FormControl, Button } from 'react-bootstrap';
 import LoginPage from '../src/components/LoginPage/LoginPage';
 import RegisterPage from '../src/components/RegisterPage/RegisterPage';
 import ProfilePage from '../src/components/ProfilePage/ProfilePage';
-import ContactPage from './components/ContactPage/ContactPage'; // Importar a página de contato
-import AboutPage from './components/AboutPage/AboutPage'; // Importar a página "Sobre"
-import './App.css';  // Importar o CSS
+import ContactPage from './components/ContactPage/ContactPage';
+import AboutPage from './components/AboutPage/AboutPage';
+import './App.css';
 
 function App() {
+  const navbarRef = useRef(null);
+
+  const handleNavLinkClick = () => {
+    if (navbarRef.current) {
+      navbarRef.current.click();
+    }
+  };
+
   return (
     <Router>
       <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar">
         <Container>
-          <Navbar.Brand href="#home" className="custom-navbar-brand">Academia</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Link to="/" className="navbar-brand custom-navbar-brand">Academia</Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" ref={navbarRef} />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto custom-nav">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/about">Sobre</Nav.Link>
-              <Nav.Link as={Link} to="/contact">Contato</Nav.Link> {/* Adicionar rota para a página de contato */}
-              <Nav.Link as={Link} to="/register">Registro</Nav.Link>
-              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/" onClick={handleNavLinkClick}>Home</Nav.Link>
+              <Nav.Link as={Link} to="/about" onClick={handleNavLinkClick}>Sobre</Nav.Link>
+              <Nav.Link as={Link} to="/contact" onClick={handleNavLinkClick}>Contato</Nav.Link>
+              <Nav.Link as={Link} to="/register" onClick={handleNavLinkClick}>Registro</Nav.Link>
+              <Nav.Link as={Link} to="/login" onClick={handleNavLinkClick}>Login</Nav.Link>
             </Nav>
             <Form className="d-flex custom-search-form">
               <FormControl
@@ -42,8 +50,8 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/contact" element={<ContactPage />} /> {/* Rota para a página de contato */}
-          <Route path="/about" element={<AboutPage />} /> {/* Rota para a página "Sobre" */}
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Container>
     </Router>
